6392649451c374115d47ee139dabf6719d95b0be
KiD Fearless <kidfearless1@gmail.com>
2020-10-11 13:50:00 -0600

    Add Notes to cheater table

--- Models/Cheater.cs
+++ Models/Cheater.cs
@@ -1,7 +1,12 @@
-﻿using SQLite;
+﻿using DangerZoneHackerTracker.Models;
+using Newtonsoft.Json;
+using SQLite;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace DangerZoneHackerTracker
 {
@@ -11,11 +16,17 @@ namespace DangerZoneHackerTracker
 		[Column(nameof(AccountID))]
 		[Unique]
 		public uint AccountID { get; set; }
+		
 		[Column(nameof(ThreatLevel))]
 		public int ThreatLevel { get; set; }
+		
 		[Column(nameof(CheatList))]
 		public string CheatList { get; set; }
+		
 		[Column(nameof(LastKnownName))]
 		public string LastKnownName { get; set; }
+
+		[Column(nameof(Notes))]
+		public string Notes { get; set; } = "";
 	}
 }
